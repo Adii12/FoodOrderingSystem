e018d9d73f48164e3808b6714864410694223494
Adrian Cimpean <cimpean_adrian12@yahoo.com>
2020-05-25 15:37:06 +0300

    Added Login and Register

---
+++ Customers's_App/app/src/main/java/com/adriancimpean/foodorder/authentication/AuthenticationActivity.kt
@@ -0,0 +1,24 @@
+package com.adriancimpean.foodorder.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.adriancimpean.foodorder.R
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    private var pager: ViewPager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+        pager = findViewById(R.id.pager)
+
+        val pageAdapter : AuthenticationFragmentAdapter = AuthenticationFragmentAdapter(supportFragmentManager)
+
+        pageAdapter.addFragment(LoginFragment())
+        pageAdapter.addFragment(RegisterFragment())
+        pager!!.adapter=pageAdapter
+    }
+}
